7e3ed0347d09c58bbeedd9aa11b36170901f7a86
Barringer <kristen.barringer@contractor.tranetechnologies.com>
2026-05-14 17:31:07 -0500

    probably not gonna work

---
+++ dbo/Scripts/PostDeployment/Data/zzz_test.sql
@@ -0,0 +1,28 @@
+ 
+
+-- post-deploy data load 
+MERGE INTO dbo.zzz_test_pipeline_20260514_1707 AS target
+USING (VALUES
+    (1, 'Sedan', 'Passenger'),
+    (2, 'Truck', 'Commercial'),
+    (3, 'Van', 'Commercial'),
+    (4, 'Motorcycle', 'Passenger')
+) AS source (ID, desc1, desc2)
+ON target.ID = source.ID
+
+WHEN MATCHED AND (
+        target.desc1 <> source.desc1
+     OR target.desc2 <> source.desc2
+    ) THEN
+    UPDATE SET
+        target.desc1 = source.desc1,
+        target.desc2 = source.desc2
+
+WHEN NOT MATCHED BY TARGET THEN
+    INSERT (ID, desc1, desc2)
+    VALUES (source.ID, source.desc1, source.desc2)
+
+WHEN NOT MATCHED BY SOURCE THEN
+    DELETE
+
+; -- the trailing semicolon is REQUIRED on MERGE
--- dbo/Tables/zzz_test_pipeline_20260514_1707.sql
+++ dbo/Tables/zzz_test_pipeline_20260514_1707.sql
@@ -1,4 +1,7 @@
 CREATE TABLE [dbo].[zzz_test_pipeline_20260514_1707]
 (
-  [Id] INT NOT NULL PRIMARY KEY
+  [Id] INT NOT NULL PRIMARY KEY,
+  desc1 varchar(50),
+  desc2 varchar(50)
 )
+                                  
